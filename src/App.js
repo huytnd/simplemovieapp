63cbd46a9cf942bc1762c1e5b28f2c263bb10ec6
Huy Tran <huytnd@hotmail.com>
2022-03-24 00:26:21 +0700

    add api key to env file, and add search function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+
+const API_URL= `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
 const App = () => {
+  const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+  useEffect(() => {
+    searchMovies('Batman');
+  }, []);
+
   return (
+    <>
     <h1>App</h1>
+    <p>{API_URL}</p>
+    </>
   )
 }
 
