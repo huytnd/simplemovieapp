33fa62c0bf14a5be5a6b68f56301b2b87691ba69
Huy Tran <huytnd@hotmail.com>
2022-03-24 00:55:55 +0700

    added MovieCard component, update App using this component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+import MovieCard from './components/MovieCard';
 
 const API_URL= `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async(title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
-  }
+    setMovies(data.Search);
+  };
+
+  const movie1 = {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+  };
+
   useEffect(() => {
     searchMovies('Batman');
   }, []);
 
   return (
-    <>
-    <h1>App</h1>
-    <p>{API_URL}</p>
-    </>
-  )
+    <div className='app'>
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input placeholder='Search for movies' 
+          value='Batman' 
+          onChange={() => {}} />
+        <img src={SearchIcon} alt='search'
+          onClick={() => {}} />
+      </div>
+      {
+        movies?.length > 0 ? (
+          <div className='container'>
+            {
+              movies.map((movie) => (
+                <MovieCard movie={movie} />
+              ))
+            }
+          </div>
+        ) : (
+          <div className='empty'>
+            <h2>No Movies found.</h2>
+          </div>
+        )
+      }
+    </div>
+  );
 }
 
 export default App
